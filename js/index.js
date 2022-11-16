0dd265f91ac9657e9854c07711f325e3bf298036
lukecode <lucassocorrosilva@gmail.com>
2022-11-16 12:51:32 -0300

    Contuidade no desafio

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
 const navItems = document.querySelector(".nav-items");
 const button = document.querySelector(".toggle")
 const close = document.querySelector(".close")
+const overlay = document.querySelector(".overlay")
 
 button.addEventListener('click', () =>{
     navItems.classList.add("show-links")
+    overlay.classList.remove("hidden")
 })
 
 close.addEventListener('click', () =>{
     navItems.classList.remove("show-links")
+    overlay.classList.add("hidden")
+
 })
